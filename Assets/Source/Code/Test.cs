ae2d9c034ed68aedaa8496b2c2f82be13f7cfe59
mofrison <mofrison@ya.ru>
2021-05-18 14:13:41 +0300

    Create example

---
+++ Assets/Source/Code/Test.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public LocalizationData localization;
+    public LocalizationController controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            controller.AddLocalization(localization.name, localization);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            controller.RemoveLocalization(localization.name);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            controller.ChangeLocalzation(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            controller.ChangeLocalzation(-1);
+        }
+    }
+
+
+}
